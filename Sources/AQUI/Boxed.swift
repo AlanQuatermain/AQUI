02aca95878dde3485d030edf33f6cca3a8f60bf4
Jim Dovey <jimdovey@mac.com>
2019-11-02 14:35:33 -0700

    Fixed availability attributes on `@Boxed`.

--- Sources/AQUI/Boxed.swift
+++ Sources/AQUI/Boxed.swift
@@ -7,7 +7,7 @@
 
 /// A property wrapper that manages persistent storage shared by copies of
 /// a value type, similar to the content of a SwiftUI Struct.
-@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0)
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 @propertyWrapper
 class Boxed<Value> {
     var wrappedValue: Value
