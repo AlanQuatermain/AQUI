f1ad934e97c986b584b757a0d2af7506be6eabfd
Jim Dovey <jimdovey@mac.com>
2019-11-15 11:09:46 -0800

    EditMode preview wrapper.

---
+++ Sources/AQUI/EditModePreviewWrapper.swift
@@ -0,0 +1,25 @@
+//
+//  EditModePreviewWrapper.swift
+//  
+//
+//  Created by Jim Dovey on 11/15/19.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, tvOS 13.0, *)
+@available(OSX, unavailable)
+@available(watchOS, unavailable)
+struct EditModePreviewWrapper<Content: View>: View {
+    @State var editMode: EditMode = .inactive
+    var content: Content
+
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        // Can't access @State outside of render loop (e.g. in preview creation)
+        content.environment(\.editMode, $editMode)
+    }
+}
