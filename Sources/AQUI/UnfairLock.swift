fb777594b7de369e2501a4943e5396cee7274079
Jim Dovey <jimdovey@mac.com>
2019-11-17 12:25:44 -0800

    Fix addressability issues of `UnfairLock`.

--- Sources/AQUI/UnfairLock.swift
+++ Sources/AQUI/UnfairLock.swift
@@ -8,18 +8,27 @@
 import os
 
 public class UnfairLock {
-    private var lock = os_unfair_lock()
+    private let lock: UnsafeMutablePointer<os_unfair_lock>
+    
+    init() {
+        lock = .allocate(capacity: 1)
+        lock.initialize(to: os_unfair_lock())
+    }
+    
+    deinit {
+        lock.deallocate()
+    }
     
     public func withLock<R>(block: () throws -> R) rethrows -> R {
-        os_unfair_lock_lock(&lock)
-        defer { os_unfair_lock_unlock(&lock) }
+        os_unfair_lock_lock(lock)
+        defer { os_unfair_lock_unlock(lock) }
         
         return try block()
     }
     
     public func tryLock<R>(block: () throws -> R) rethrows -> R? {
-        if os_unfair_lock_trylock(&lock) {
-            defer { os_unfair_lock_unlock(&lock) }
+        if os_unfair_lock_trylock(lock) {
+            defer { os_unfair_lock_unlock(lock) }
             return try block()
         }
         return nil
