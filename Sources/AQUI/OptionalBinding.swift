5089518fe2083254a2100373fd789fd78ebec6d3
Jim Dovey <jimdovey@mac.com>
2020-01-06 21:10:33 -0800

    OptionalBinding is now public, with availability attributes.

--- Sources/AQUI/OptionalBinding.swift
+++ Sources/AQUI/OptionalBinding.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-extension Binding {
+public extension Binding {
     /// Given a binding to an optional value, creates a non-optional binding that projects
     /// the unwrapped value. If the given optional binding contains `nil`, then the supplied
     /// value is assigned to it before the projected binding is generated.
@@ -25,6 +25,7 @@ extension Binding {
     ///             TextField("Name", text: Binding($thing.name, ""))
     ///         }
     ///     }
+    @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
     init(_ source: Binding<Value?>, _ defaultValue: Value) {
         // Ensure the source doesn't contain nil.
         if source.wrappedValue == nil {
@@ -37,13 +38,14 @@ extension Binding {
     /// Creates a binding that projects the result of `source.wrappedValue == nil`. Additionally
     /// takes a default value: this will be used to set the underlying binding's value when this binding's
     /// wrapped value is set to `true`.
+    @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
     init<T>(isNotNil source: Binding<T?>, defaultValue: T) where Value == Bool {
         self.init(get: { source.wrappedValue != nil },
                   set: { source.wrappedValue = $0 ? defaultValue : nil })
     }
 }
 
-extension Binding where Value: Equatable {
+public extension Binding where Value: Equatable {
     /// Creates a non-nil binding by projecting to its unwrapped value, translating nil values
     /// to or from the given nil value. If the source contains nil, this binding will return the
     /// nil value. If this binding is set to the given nil value, it will assign nil to the underlying
@@ -58,6 +60,7 @@ extension Binding where Value: Equatable {
     ///
     /// If the `name` property contains `nil`, the text field will see an empty string. If the text field
     /// assigns an empty string, the `name` property will be set to `nil`.
+    @available(macOS 10.15, iOS 13, tvOS 13, watchOS 6, *)
     init(_ source: Binding<Value?>, replacingNilWith nilValue: Value) {
         self.init(
             get: { source.wrappedValue ?? nilValue },
